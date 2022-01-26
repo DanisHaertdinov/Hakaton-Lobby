0c032ad98185654ef158c4e14537f87cb0937443
Danis <danis.haertdinov@gmail.com>
2022-01-26 20:12:08 +0400

    first steps

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,9 +5,14 @@ type Data = {
   name: string
 }
 
+const users = [{ name: 'John Doe' }]
+
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === 'POST') {
+    users.push({name: req.body})
+  }
+  res.status(200).json(users)
 }
