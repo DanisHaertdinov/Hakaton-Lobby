cbceb621ec882976ab500a1e4a1eeed7ffda693e
DanisHaertdinov <41995780+DanisHaertdinov@users.noreply.github.com>
2024-10-16 22:01:59 +0400

    Update src/pages/api/mongo/session/model.ts

--- src/pages/api/mongo/session/model.ts
+++ src/pages/api/mongo/session/model.ts
@@ -2,4 +2,5 @@ import { Session as SessionType } from "../../../../types";
 import { createMongoModel } from "../utils";
 import { sessionSchema } from "./schema";
 
+// TODO: перенести mongo из api/mongo в src/service/mongo
 export const Session = createMongoModel<SessionType>("Session", sessionSchema);
