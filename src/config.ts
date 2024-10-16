17367d2885bccd183db410a172bd8eadc69feb06
DanisHaertdinov <41995780+DanisHaertdinov@users.noreply.github.com>
2024-10-16 22:37:46 +0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export const gitHubToken = process.env.HACK_LOBBY_GITHUB_TOKEN as string;
 export const vercelToken = process.env.HACK_LOBBY_VERCEL_TOKEN as string;
 export const gitHubID = process.env.GITHUB_OAuth_ID as string;
 export const gitHubSecret = process.env.GITHUB_OAuth_SECRET as string;
-export const DOMAIN = process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000";
+export const DOMAIN = process.env.NEXT_PUBLIC_VERCEL_URL || "";
 export const HACK_MONGO_USER = process.env.HACK_MONGO_USER as string;
 export const HACK_MONGO_PASS = process.env.HACK_MONGO_PASS as string;
